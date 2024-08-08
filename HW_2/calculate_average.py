9e0d7367b48aa085aa33a1c758d18528fa13409e
skvlan <skvland052@gmail.com>
2024-08-08 20:19:00 +0200

    Added HM_2 fixed

--- HW_2/calculate_average.py
+++ HW_2/calculate_average.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
 import pandas as pd
+import random
+import string
+
 
 app = Flask(__name__)
 
@@ -9,14 +12,14 @@ def calculate_average():
 
     df.columns = df.columns.str.strip()
 
-    column_name = request.args.get('column', default='Height(Inches)')
-    column_name_1 = request.args.get('column1', default='Weight(Pounds)')
+    column_name = "Height(Inches)"
+    column_name_1 = "Weight(Pounds)"
 
     if column_name not in df or column_name_1 not in df:
         return "Column doesn't found", 400
 
-    average_height = df[column_name].mean()
-    average_weight = df[column_name_1].mean()
+    average_height = df["Height(Inches)"].mean()
+    average_weight = df["Weight(Pounds)"].mean()
 
     results = (f"Average value for {column_name} = {average_height}\n |"
                f" Average value for {column_name_1} = {average_weight}")
@@ -25,11 +28,36 @@ def calculate_average():
 
 
 
-if __name__ == '__main__':
-    app.run(
-        port=5001, debug=True
+@app.route("/generate-password")
+def generate_password():
+    password_length = random.randint(10, 20)
+
+    password = [
+        random.choice(string.ascii_uppercase),
+        random.choice(string.digits),
+        random.choice(string.punctuation)
+    ]
+
+    if password_length > 3:
+        password += random.choices(
+            string.ascii_letters + string.digits + string.punctuation,
+            k=password_length - 3
+        )
+
+    random.shuffle(password)
+
+    return "".join(
+        random.choices(
+            string.digits + string.ascii_letters + string.punctuation, k = password_length
+        )
     )
 
+
+
+if __name__ == '__main__':
+    app.run(
+        port=5000, debug=True
+    )
 
 
 
